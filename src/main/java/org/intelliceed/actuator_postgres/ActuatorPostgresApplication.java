4def66a5ead4cc8921121b8387e95d96eff8d932
Danil <39278353+Multizone@users.noreply.github.com>
2020-02-18 13:45:01 +0200

    qwe
    qwe

---
+++ src/main/java/org/intelliceed/actuator_postgres/ActuatorPostgresApplication.java
@@ -0,0 +1,13 @@
+package org.intelliceed.actuator_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/intelliceed/actuator_postgres/ActuatorPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intelliceed.actuator_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
